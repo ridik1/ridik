ca3df9fe769708d9f93780524cfd51f98d8709dc
ridik1 <radikulit09@gmail.com>
2019-04-01 16:39:42 +0300

    ridik2

--- Project1/CodeFile1.cs
+++ Project1/CodeFile1.cs
@@ -15,4 +15,57 @@ namespace L5
         }
 
 
-  
+        static void Kvit()
+        {
+            Kvit kvit = new Kvit();
+            kvit.SetInfo();
+            kvit.GetInfo();
+            Console.WriteLine();
+
+
+        }
+    }
+    class Kvit
+
+    {
+        public int Number { get; set; }
+        public string Data { get; set; }
+        public float Sum { get; set; }
+
+        public Kvit(int number, string data, float sum)
+        {
+            Number = number;
+            Data = data;
+            Sum = sum;
+        }
+
+        public Kvit()
+        {
+            Number = 0;
+            Data = "";
+            Sum = 0;
+        }
+
+        public void SetInfo()
+        {
+            Console.WriteLine("Ввод инфо о изделии: ");
+
+            Console.Write("Номер: ");
+            Number = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Дата: ");
+            Data = Convert.ToString(Console.ReadLine());
+
+            Console.Write("Сумма: ");
+            Sum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"Номер: {Number}");
+            Console.WriteLine($"Дата: {Data}");
+            Console.WriteLine($"Сумма: {Sum}");
+        }
+    }
+}
